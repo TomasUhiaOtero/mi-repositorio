c5510a436445493a72c65ff59b83f66b03d3c3be
Tomas <tomasuhiaotero@gmail.com>
2025-02-06 12:44:57 +0100

    Realizacion Ejercicio 1.3

--- part1/Exercise 1/Program/src/App.jsx
+++ part1/Exercise 1/Program/src/App.jsx
@@ -1,44 +1,40 @@
-const Header = ({ titulo }) => {
-  return <h1>{titulo}</h1>;
-};
+const Header = ({ title }) => <h1>{title}</h1>;
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name}: {exercises}
-    </p>
-  );
-};
+const Part = ({ name, exercises }) => (
+  <p>
+    {name}: {exercises}
+  </p>
+);
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+
+const Content = ({ parts }) => {
   return (
     <>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
     </>
   );
 };
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  const totalExercises = exercises1 + exercises2 + exercises3;
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
   return <p>Total exercises: {totalExercises}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <>
-      <Header titulo={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header title={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   );
 };
