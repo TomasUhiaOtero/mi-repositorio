77f2c15fd0c67acc02c58ff2fd64019dac0236c5
Tomas <tomasuhiaotero@gmail.com>
2025-02-09 13:25:01 +0100

    Manejadores de eventos

--- part1/Pruebas/prueba/src/App.jsx
+++ part1/Pruebas/prueba/src/App.jsx
@@ -1,25 +1,22 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear - age
-
-  return (
-    <>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+     <button onClick={setToZero}> 
+      zero
+     </button>
     </>
   )
 }
 
-export default App;
+export default App
