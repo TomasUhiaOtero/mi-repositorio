09f0d9a78677d21470dc04bfeb3bee8799f766fa
Tomas <tomasuhiaotero@gmail.com>
2025-02-14 13:11:18 +0100

    Reorganized files and folders

--- part1/Pruebas/prueba/src/App.jsx
+++ part1/Pruebas/prueba/src/App.jsx
@@ -1,54 +1,38 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+// Este es lugar correcto para definir un componente
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
+const Display = props => <div>{props.value}</div>
+
 const App = () => {
+  const [value, setValue] = useState(10)
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left+1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft+right)
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right+1
-    setRight(right + 1)
-    setRight(updatedRight)
-    setTotal(updatedRight+left)
+  const resetLeftAndRight = () => {
+    setLeft(0)
+    setRight(0)
   }
+ 
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <Button handleClick={resetLeftAndRight} text="reset left and right" />
+
     </div>
   )
 }
