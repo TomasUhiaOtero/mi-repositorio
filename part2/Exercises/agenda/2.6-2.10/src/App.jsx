43b4c85f6cc0e460cc6e1ecca58e14ae9bcd9f9c
Tomas <tomasuhiaotero@gmail.com>
2025-02-24 17:39:38 +0100

    Realizacion 2.6-2.10

--- part2/Exercises/agenda/2.6-2.10/src/App.jsx
+++ part2/Exercises/agenda/2.6-2.10/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Header from './components/Header';
+import Formulary from './components/Formulary';
+import SearchBar from './components/SearchBar';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if(!newName || !newNumber) {
+      alert('Please fill in all fields');
+      return;
+    }
+    if (persons.some(person => person.name === newName || person.number === newNumber )) {
+      alert(`${newName} or ${newNumber} is already added to the phonebook`);
+      return;
+    }
+
+  
+    const newPerson = { name: newName, number: newNumber };
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const filteredPersons = persons.filter(person => 
+    person.name.toLowerCase().includes(search.toLowerCase()) || person.number.includes(search)
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header Title="Phonebook." />
+    <SearchBar search={search} setSearch={setSearch}/>
+      <Header Title="Add a new Contact." />
+      <Formulary
+        newName={newName}
+        setNewName={setNewName}
+        addPerson={addPerson}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+      <Header Title="Numbers." />
+      <ul>
+        {filteredPersons.map((person) => (
+          <li key={person.name}>
+            {person.name} - {person.number}
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
